0d8dfe178763996fba40c93a8121a1ea36f82689
Marina Garcia <marinaelizabethnavi@gmail.com>
2026-03-08 13:40:40 -0700

    Mi primer commit well second after installing everything again

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-3fb88c50-811b-4c7c-8610-c3bddc7cdb5c.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
